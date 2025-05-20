55de7fa93acbca066a553c0a37f6d35f6e798f3b
Vlada <vladislavassskupa@gmail.com>
2025-05-20 09:47:50 +0300

    (not)laast touches

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv';
-import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from "./config/cloudinary.js"
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
-import orderRouter from './routes/orderRoute.js';
+import orderRouter from './routes/orderRoute.js'
 
 // App Config
 dotenv.config();
 const app = express()
+
 const port = process.env.PORT || 4000
 
 // middlewares
@@ -39,8 +39,5 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: 'Щось пішло не так!' });
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(port, () => console.log(`Сервер працює на порті: ${port}`));
-}
 
-export default app;
+app.listen(port, () => console.log(`Сервер працює на порті: ${port}`));
