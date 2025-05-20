ab8d89a66870804ef8110d33083d049d108f78bd
Vlada <vladislavassskupa@gmail.com>
2025-05-20 09:00:34 +0300

    Finishing

--- pet-shop/src/components/RelatedProducts.jsx
+++ pet-shop/src/components/RelatedProducts.jsx
@@ -22,7 +22,7 @@ const RelatedProducts = ({category, subCategory}) => {
   return (
     <div className='my-24'>
       <div className='text-center text-3xl py-2'>
-        <h1>Related Products</h1>
+        <h1>Вас також можуть зацікавити:</h1>
       </div>
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
       {related.map((item, index) => (
